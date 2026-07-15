f436b6dc3f6a439787c63537320bc9c7641f450d
Hamish McLean <HamishMcLean94@gmail.com>
2026-07-15 10:20:00 +0100

    Setup project

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Development flake for tide";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
+      system:
+      let
+        # pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; };
+        buildInputs = with pkgs; [
+          # stdenv.cc.cc.lib
+          # zlib
+          # glib
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python3
+            uv
+            ruff
+          ];
+
+          inherit buildInputs;
+
+          shellHook = ''
+            # Only set up library paths for Nix users so compiled wheels work
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+          '';
+        };
+      }
+    );
+}
