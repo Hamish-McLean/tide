57bc3fbcdd938a023f62f16ec61842a3799d284d
Hamish McLean <HamishMcLean94@gmail.com>
2026-07-17 09:30:52 +0100

    Fix: inky display formatting

--- src/tide/inky.py
+++ src/tide/inky.py
@@ -4,25 +4,27 @@ from PIL import Image
 from tide.utils import get_project_root
 
 
-def initialise_inky_display(border=inky_display.WHITE, invert=False):
+def initialise_inky_display(invert=False):
     inky_display = auto()
-    inky_display.set_border(border)
+    inky_display.set_border(inky_display.WHITE)
 
     # Invert screen
     inky_display.h_flip = invert
     inky_display.v_flip = invert
 
+    return inky_display
 
-def display_image(image: Image.Image):
-    inky.set_image(image)
+
+def display_image(inky_display, image: Image.Image):
+    inky_display.set_image(image)
 
 
 if __name__ == "__main__":
     project_root = get_project_root()
     cache_dir = project_root / ".cache"
 
-    initialise_inky_display(invert=True)
+    inky_display = initialise_inky_display(invert=True)
 
     image = Image.open(cache_dir / "tide_plot.png")
 
-    display_image(image)
+    display_image(inky_display, image)
