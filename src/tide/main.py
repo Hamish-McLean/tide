1c030b59e193bf20b51ffcd9e3bfb6476fc7bba2
Hamish McLean <HamishMcLean94@gmail.com>
2026-07-18 20:38:37 +0100

    feat: add main function to run the whole pipeline

--- src/tide/main.py
+++ src/tide/main.py
@@ -1,5 +1,37 @@
+import os
+import sys
+
+from tide import api, inky, plot
+from tide.plot import plot_tides
+
+
 def main():
-    print("Hello from tide!")
+    # Get tide data from Admiralty Discovery API
+    API_KEY = os.getenv("ADMIRALTY_DISCOVERY_API_KEY")
+    STATION = "0023C"  # Totnes
+
+    if not API_KEY:
+        print(
+            "[ERROR] ADMIRALTY_DISCOVERY_API_KEY environment variable is not set!",
+            file=sys.stderr,
+        )
+        exit(1)
+    try:
+        tide_data = api.fetch_admiralty_tides(API_KEY, STATION)
+    except Exception as e:
+        print(f"[ERROR] API fetch failed: {e}")
+        exit(1)
+
+    # Plot tide data
+    try:
+        tide_plot = plot_tides(tide_data)
+    except Exception as e:
+        print(f"[ERROR] Plotting failed {e}")
+        exit(1)
+
+    # Draw plot to inky phat display
+    inky_display = inky.initialise_inky_display(invert=True)
+    inky.display_image(inky_display, tide_plot)
 
 
 if __name__ == "__main__":
